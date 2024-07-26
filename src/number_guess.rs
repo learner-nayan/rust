dbe2b166296d609b27ddb34ce945780a3c6cef0d
Nayan <khanalnayan123@gmail.com>
2024-07-27 00:01:05 +0545

    number guessing game with exception handling

--- src/number_guess.rs
+++ src/number_guess.rs
@@ -3,27 +3,35 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 pub fn number_guess(){
-    let mut guess = String::new();
-
     let secret_number = rand::thread_rng().gen_range(1..=101);
     println!("The secret number is {}",secret_number);
 
     println!("Guess the number");
-    println!("Enter the number:");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        let mut guess = String::new();
+        println!("Enter the number:");
 
-    let guess:u32 = guess.trim()
-        .parse().expect("Please type a number");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed {}",guess);
-    // println!("The secret number is {}",secret_number);
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("You guessed low!"),
-        Ordering::Greater => println!("You guessed high!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed {}",guess);
+        // println!("The secret number is {}",secret_number);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("You guessed low!"),
+            Ordering::Greater => println!("You guessed high!"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
     }
+
 }
