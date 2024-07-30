e33873ffae09826dafb396419e9c2cfce16e32ac
Nayan <khanalnayan123@gmail.com>
2024-07-31 00:09:36 +0545

    enums in rust

--- src/enums.rs
+++ src/enums.rs
@@ -1,3 +1,5 @@
+use crate::enums::UsState::Alaska;
+
 enum IpAddrType{
     v4(u8,u8,u8,u8),
     v6(String)
@@ -22,6 +24,7 @@ struct MoveMessage{
 //     None,
 // }
 
+#[derive(Debug)]
 enum UsState{
     Albana,
     Alaska,
@@ -34,7 +37,7 @@ enum Coin{
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 pub fn enums(){
@@ -42,7 +45,7 @@ pub fn enums(){
 
     let x = Some(5);
     let y = Some("Hello");
-    let z: Option<u32> = None;
+    let z: Option<i32> = None;
 
     let num1 = 5;
     let num2 = Some(10);
@@ -53,4 +56,44 @@ pub fn enums(){
     let result2 = num1 + num3.unwrap_or(0);
     println!("{}",result1);
     println!("{}",result2);
+
+    let amount = value_in_cents(Coin::Quarter(UsState::Alaska));
+    println!("{}",amount);
+
+    let a:Option<i32>  = Some(5);
+    let b:Option<i32> = None;
+
+    let res1 = add_one(a);
+    println!("{:?}",res1);
+    let res2 = add_one(b);
+    println!("{:?}",res2);
+}
+
+fn value_in_cents(coin: Coin) -> u8{
+    match coin {
+        Coin::Penny => {
+            println!("Penny");
+            1
+        },
+        Coin::Nickel => {
+            println!("Nickel");
+            5
+        },
+        Coin::Dime => {
+            println!("Dime");
+            10
+        },
+        Coin::Quarter(state) => {
+            println!("Quarter: {:?}",state);
+            25
+        }
+    }
+}
+
+fn add_one(g: Option<i32>) -> Option<i32>{
+    match g {
+        // None => None,
+        Some(i) => Some(i+1),
+        _ => None,
+    }
 }
